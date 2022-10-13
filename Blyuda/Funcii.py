109841bd19badb894709e4b0b1938c4d7359544a
Сирожиддин <86679346+Sirojidd@users.noreply.github.com>
2022-10-13 13:38:57 +0300

    Add files via upload

---
+++ Blyuda/Funcii.py
@@ -0,0 +1,14 @@
+class Func():
+    back = []
+    search = []
+    agent = []
+
+
+
+    def back(self):
+        pass
+
+
+
+    def agent(self):
+        pass
---
+++ Blyuda/nationals.py
@@ -0,0 +1,17 @@
+from tkinter import Tk, Frame, Button
+
+class Nat(Tk):
+
+
+    def __init__(self):
+        nat = self.nat
+        nat.frm = Frame(text="Национальные блюда", )
+        nat.frm.pack
+        nat.btn = Button(text="")
+        nat.btn.pack
+        nat.btn2 = Button()
+        nat.btn2()
+        nat.btn3 = Button()
+        nat.btn3.pack
+        nat.btn4 = Button(text="")
+        nat.btn5.pack
